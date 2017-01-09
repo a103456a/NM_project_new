c2300041c5ab321ac2f2a97c81f115af5718c1a9
Alan Chiang <a103456a@hotmail.com>
2017-01-10 01:01:01 +0800

    秀一點點資訊
    型號
    名稱
    連線

--- app/js/index.js
+++ app/js/index.js
@@ -1,26 +1,148 @@
 'use strict';
 
+let snmp = require('snmp-native');
+let HOST = '192.168.0.1';
+let session;
 
-let snmp = require('./../utilities/snmp.js');
+startUp();
 
-snmp.get('.1.3.6.1.2.1.1.3.0', '120.107.172.237')
-    .then((result) => {
+
+// action listeners
+const ipc = require('electron').ipcRenderer;
+
+let closeEl = document.getElementById('close');
+closeEl.addEventListener('click', function () {
+    ipc.send('close-main-window');
+});
+
+let showHostName = document.getElementById('showHostName');
+showHostName.addEventListener('click', function () {
+    document.getElementById('edit-hostName').style.display = '';
+    document.getElementById('editHostName').style.display = '';
+
+    let dvcName = document.getElementById('deviceName');
+    dvcName.style.display = 'none';
+
+    document.getElementById('showHostName').style.display = 'none';
+});
+
+let editHostName = document.getElementById('editHostName');
+editHostName.addEventListener('click', function () {
+    let dvcName = document.getElementById('edit-hostName');
+
+    session.set({ oid: '.1.3.6.1.2.1.1.5.0', value: dvcName.value, type: 4 }, (error, result) => {
+        if (!error) {
+            alert('修改成功!');
+            overview();
+        } else {
+            alert('修改失敗，請再試一次');
+        }
+    });
+});
+
+let submitHostIP = document.getElementById('submitHostIP');
+submitHostIP.addEventListener('click', function () {
+    HOST = document.getElementById('hostIP').value;
+    connectHost();
+});
+
+let reconnect = document.getElementById('reconnect');
+reconnect.addEventListener('click', function () {
+    startUp();
+});
+
+
+// utility functions
+// initialize overview
+function overview() {
+    // show agent IP
+    let showIP = document.getElementById('showIP');
+    showIP.innerText = HOST;
+
+    // get model name
+    session.get({ oid: '.1.3.6.1.2.1.1.1.0' }, (error, result) => {
+        let mdlName = document.getElementById('modelName');
+        mdlName.innerText = result[0].value;
+    });
+
+    // get device name
+    session.get({ oid: '.1.3.6.1.2.1.1.5.0' }, (error, result) => {
+        document.getElementById('showHostName').style.display = '';
+        document.getElementById('editHostName').style.display = 'none';
+
+
+        let editName = document.getElementById('edit-hostName');
+        editName.style.display = 'none';
+
+        let dvName = document.getElementById('deviceName');
+        dvName.style.display = '';
+        dvName.innerText = result[0].value;
+        editName.value = result[0].value;
+    });
+
+    // get system up time
+    session.get({ oid: '.1.3.6.1.2.1.1.3.0' }, (error, result) => {
         var upt = document.getElementById('uptime');
         upt.innerText = upTime(result[0].value);
 
         let q = result[0].value;
         window.setInterval(() => {
-            q += 100
-            upt.innerText = upTime(q)
+            q += 100;
+            upt.innerText = upTime(q);
         }, 1000);
     });
+}
+
+// initialize port information
+function portInfo() {
+    let OID = '.1.3.6.1.2.1.4.3.0';
+
+    let data = new Array();
+    let temp = {
+        ipNo: '無法取得',
+        inRcv: 0
+    }
+    for (let i = 0; i < 8; i++) {
+        data.push(temp);
+    }
 
 
-const ipc = require('electron').ipcRenderer;
-let closeEl = document.getElementById('close');
-closeEl.addEventListener('click', function () {
-    ipc.send('close-main-window');
-});
+    // get port table
+    for (let i = 0; i < 8; i++) {
+        session.get({ oid: OID }, (error, result) => {
+            data[i].inRcv = result[0].value;
+        });
+    }
+
+
+    $('.portTable').empty();
+    for (let i = 0; i < 8; i++) {
+        $('.portTable').append(
+            `<li>
+                <div class="collapsible-header">Port ${i + 1}</div>
+                <div class="collapsible-body">
+                    <p>IP位置: ${data[i].ipNo}</p>
+                    <p>${data[i].inRcv}</p>
+                </div>
+             </li>`);
+    }
+}
+
+function startUp() {
+    document.getElementById('connectHost').style.display = '';
+    document.getElementById('mainPage').style.display = 'none';
+    document.getElementById('hostIP').value = HOST;
+}
+
+function connectHost() {
+    session = new snmp.Session({ host: HOST, community: 'public' })
+
+    document.getElementById('mainPage').style.display = '';
+    document.getElementById('connectHost').style.display = 'none';
+
+    overview();
+    portInfo();
+}
 
 
 function upTime(timeTick) {
@@ -33,5 +155,5 @@ function upTime(timeTick) {
     time[2] = Math.floor(timeTick % 3600 / 60);
     time[3] = Math.floor(timeTick % 60);
 
-    return `${time[0]} 日 ${time[1]} 時 ${time[2]} 分 ${time[3]} 秒`;
+    return `${time[0]} 日 ${time[1]} 小時 ${time[2]} 分 ${time[3]} 秒`;
 }
--- main.js
+++ main.js
@@ -17,7 +17,6 @@ let mainWindow;
 function createWindow() {
   // Create the browser window.
   mainWindow = new BrowserWindow({
-    frame: false,
     width: 1024,
     height: 768
   });
@@ -30,7 +29,7 @@ function createWindow() {
   }));
 
   // Open the DevTools.
-  //mainWindow.webContents.openDevTools();
+  mainWindow.webContents.openDevTools();
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function () {
